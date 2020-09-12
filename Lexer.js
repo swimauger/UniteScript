de19d34b43651acf755802c329a6dc5da25e9551
swimauger <swimauger@gmail.com>
2020-09-12 15:26:29 -0500

    Add Lexer

---
+++ Lexer.js
@@ -0,0 +1,74 @@
+class Lexer {
+    static isNumber(symbol) {
+        return !!Number(symbol);
+    }
+
+    static isString(symbol) {
+        return /"|'|`/.test(symbol);
+    }
+
+    static isKeyword(symbol) {
+        return symbol === 'Log';
+    }
+
+    static isIdentifier(symbol) {
+        return /[A-Z]*/.test(symbol);
+    }
+
+    static isOperator(symbol) {
+        return /O|U|\?/.test(symbol);
+    }
+
+    static evaluate(line) {
+        const symbols = [];
+        let str = "";
+        let strType = "";
+        for (let word of line.split(' ')) {
+            let endPunctuation = false;
+            if (word[0] === '(') {
+                symbols.push({ type: 'punctuation', value: '(' });
+                word = word.substr(1, word.length);
+            }
+            if (word[word.length-1] === ')') {
+                endPunctuation = true;
+                word = word.substr(0, word.length-1);
+            }
+
+            if (this.isNumber(word)) {
+                symbols.push({ type: 'number', value: word });
+            } else if (this.isOperator(word)) {
+                symbols.push({ type: 'operator', value: word });
+            } else if (this.isIdentifier(word)) {
+                if (this.isString(word[0]) || this.isString(word[word.length-1])) {
+                    if (word[0] === word[word.length-1]) {
+                        symbols.push({ type: 'string', value: word.substr(1, word.length-1) });
+                    } else if (this.isString(word[0])) {
+                        strType = word[0];
+                        str += word.substr(1, word.length);
+                    } else if (word[word.length-1] === strType) {
+                        str += ` ${word.substr(0, word.length-1)}`;
+                        symbols.push({ type: 'string', value: str });
+                    } else {
+                        str += ` ${word}`;
+                    }
+                } else if (str) {
+                    str += ` ${word}`;
+                } else {
+                    if (this.isKeyword(word)) {
+                        symbols.push({ type: 'keyword', value: word });
+                    } else {
+                        symbols.push({ type: 'identifier', value: word });
+                    }
+                }
+            }
+
+            if (endPunctuation) {
+                symbols.push({ type: 'punctuation', value: ')' })
+            }
+            
+        }
+        return symbols;
+    }
+}
+
+module.exports = Lexer;
